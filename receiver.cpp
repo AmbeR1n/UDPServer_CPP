5dfdd3150b230282add2c2746b1e0f13261180e7
elisey <karkavin05@gmail.com>
2023-10-30 10:32:33 +0700

    buffer 24k+512+256-128-64+32-16

--- receiver.cpp
+++ receiver.cpp
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    const int BUFFER_SIZE = 24*1024+512+256-128-64+32;
+    const int BUFFER_SIZE = 24*1024+512+256-128-64+32-16;
     const int S_PORT = std::strtol(argv[1], nullptr, 10);
 
     
